804393723ddf5b54967c1fa1d689353c961da070
Franklin <franklinmatos8@gmail.com>
2024-10-18 05:18:06 -0300

    gruvbox-material

--- lua/user/config/colorscheme.lua
+++ lua/user/config/colorscheme.lua
@@ -1,30 +1,35 @@
+-- better vscode theme
+-- local M = {
+-- 	"Mofiqul/vscode.nvim",
+-- 	priority = 1000,
+-- 	opts = {
+-- 		-- Enable italic comment
+-- 		italic_comments = true,
+--
+-- 		-- Underline `@markup.link.*` variants
+-- 		underline_links = true,
+--
+-- 		-- Disable nvim-tree background color
+-- 		disable_nvimtree_bg = false,
+--
+-- 		-- Override colors (see ./lua/vscode/colors.lua)
+-- 		color_overrides = {
+-- 			-- vscLineNumber = '#FFFFFF',
+-- 		},
+-- 		-- Override highlight groups (see ./lua/vscode/theme.lua)
+-- 		group_overrides = {
+-- 			-- this supports the same val table as vim.api.nvim_set_hl
+-- 			-- use colors from this colorscheme by requiring vscode.colors!
+-- 		},
+-- 	},
+-- }
 local M = {
-	"Mofiqul/vscode.nvim",
-	priority = 1000,
-	opts = {
-		-- Enable italic comment
-		italic_comments = true,
-
-		-- Underline `@markup.link.*` variants
-		underline_links = true,
-
-		-- Disable nvim-tree background color
-		disable_nvimtree_bg = false,
-
-		-- Override colors (see ./lua/vscode/colors.lua)
-		color_overrides = {
-			-- vscLineNumber = '#FFFFFF',
-		},
-		-- Override highlight groups (see ./lua/vscode/theme.lua)
-		group_overrides = {
-			-- this supports the same val table as vim.api.nvim_set_hl
-			-- use colors from this colorscheme by requiring vscode.colors!
-		},
-	},
+	"sainnhe/gruvbox-material",
+	priority = 1000, -- Load it before all the other start plugins.
 }
 
 function M.config()
-	vim.cmd.colorscheme("vscode")
+	vim.cmd.colorscheme("gruvbox-material")
 end
 
 return M
